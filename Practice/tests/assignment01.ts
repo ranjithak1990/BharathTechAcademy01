3e6754f79903bade2e97cedb45f113977e597701
ranjithak1990 <ranjithak1990@gmail.com>
2026-02-11 07:02:57 +0530

    'Assignment01_Day1'

---
+++ Practice/tests/assignment01.ts
@@ -0,0 +1,61 @@
+//Temperature of a city in degrees Celsius: 25.5
+let temp:number =25.5;
+console.log(temp)
+// Whether a customer has placed an order: true or false
+let order:boolean =true
+console.log(order)
+// Person's phone number: "123-456-7890"
+let phoneNumber:string ="123-456-7890"
+console.log(phoneNumber)
+//Amount of money in a customer's bank account: 1000.50
+let money:number = 1000.50
+console.log(money)
+// Person's email address: "john.doe@example.com"
+let mail:string="john.doe@example.com"
+console.log(mail)
+//Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+interface locationint{latitude:number,longitude:number}
+let location1:locationint={latitude:37.7749,longitude:-122.4194}
+console.log(location1)
+//Person's marital status: true or false
+let marital_Status:boolean=true
+console.log(marital_Status)
+//Person's occupation: "Software Engineer"
+let occupation:string="Software Engineer"
+console.log(occupation)
+//Person's favourite colour: "Blue"
+let color:string="Blue"
+console.log(color)
+//Current year: 2023
+let yr:number=2023
+console.log(yr)
+//Number of followers on a social media platform: 1,000,000
+let follower:string = '1,000,000'
+console.log(follower)
+//Rating of a movie: 7.5
+let rating:number=7.5
+console.log(rating)
+//Person's blood type: 'A'
+let bloodtype:string='A'
+console.log(bloodtype)
+//Title of a book: "To Kill a Mockingbird"
+let titleofBook:string="To Kill a Mockingbird"
+console.log(titleofBook)
+//Number of employees in a company: 500
+let NumOfEmp:number =500
+console.log(NumOfEmp)
+//Time of an event: 2:30 PM
+let evenTime:string="2.30 PM"
+console.log(evenTime)
+//Name of a country: "United States"
+let country:string="United States"
+console.log(country)
+//Person's eye color: "Brown"
+let eyecolor:string="Brown"
+console.log(eyecolor)
+//Person's birthplace: "New York City"
+let birthplace:string="New York City"
+console.log(birthplace)
+// Distance between two cities: 200.5
+let distence:number=200.5
+console.log(distence)
